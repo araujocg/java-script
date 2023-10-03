b5dd1aeb60703d31d03381a7cda97f2eb33c4859
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-03 06:17:52 -0400

    calculoimc 0.2 03/10/23

--- CursoJS/calculandoIMC/script.js
+++ CursoJS/calculandoIMC/script.js
@@ -1,13 +1,19 @@
 function Calcular(){
-    let peso = document.getElementById('peso')
-    let altura = document.getElementById('altura')
+    let peso = Number(document.getElementById('peso').value)
+    let altura = Number(document.getElementById('altura').value)
+    let main = document.getElementById('main')
     let res = document.getElementById('res')
-    let imc = Number(peso /(altura ** 2))
-    console.log(peso)
-    if (imc<18.5){
-        res.innerHTML = `Abaixo do peso ideal`
-    } else if (){
-
+    let imc = (peso /(altura ** 2))
+    let mensagens = {
+        'Abaixo do peso': (imc<18.5),
+        'Peso Normal': (imc>=18.5 && imc<24.9),
+        'Sobrepeso': (imc>=25 && imc<29.9),
+        'Obesidade grau I':(imc>=30 && imc<34.9),
+        'Obesidade grau II':(imc>=35 && imc<39.9),
+        'Obesidade grau III':(imc>=40)
     }
-
+    let mensagem = Object.keys(mensagens).find(key => mensagens[key])
+    res.innerHTML = mensagem || 'Valor de IMC não reconhecido'
+    res.style.display = 'flex'
+    main.style.height = '90%'
 }
