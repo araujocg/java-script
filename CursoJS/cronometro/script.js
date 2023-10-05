eeb19e56ecbf705f4c7ff09ded2f42d51c41b645
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-05 06:43:28 -0400

    cronometro 0.2 05/10/23

--- CursoJS/cronometro/script.js
+++ CursoJS/cronometro/script.js
@@ -1,15 +1,21 @@
-let milisegundos = 0
+let cronometro = document.getElementById('cronometro')
 let segundos = 0
 let minutos = 0
-const cronometro = setInterval(function(){
-	console.log(`${segundos} : ${milisegundos} segundos passaram`)
-	milisegundos++
-	if (milisegundos>=60){
-		milisegundos = 0
+let horas = 0
+function iniciarCronometro(){
+	setInterval(function(){
+		cronometro.innerHTML =`${horas} : ${minutos} : ${segundos}`
 		segundos++
-	}
-	if (segundos>=30){
-		clearTimeout(cronometro)
-		console.log('Os 30 segundos se passaram')
-	}
-},1)
+		if (segundos<10){
+			segundos = [`0${segundos}`]
+		}
+		if (segundos>=60){
+			segundos = 0
+			minutos++
+		}
+		if(minutos>=60){
+			minutos = 0
+			horas++
+		}
+	},1000)
+}
