9a8a3481f6fd3cef6067bb509e6ce0b7b3c03ccb
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-06 07:32:58 -0400

    cronometro 0.3 06/10/23

--- CursoJS/cronometro/script.js
+++ CursoJS/cronometro/script.js
@@ -1,21 +1,47 @@
 let cronometro = document.getElementById('cronometro')
-let segundos = 0
+let segundos = 1
 let minutos = 0
 let horas = 0
-function iniciarCronometro(){
-	setInterval(function(){
-		cronometro.innerHTML =`${horas} : ${minutos} : ${segundos}`
-		segundos++
-		if (segundos<10){
-			segundos = [`0${segundos}`]
-		}
-		if (segundos>=60){
-			segundos = 0
-			minutos++
-		}
-		if(minutos>=60){
-			minutos = 0
-			horas++
-		}
-	},1000)
+let intervalo
+let emExecucao = false
+
+function formatarNumero(numero){
+	if(numero<10){
+		return numero.toString().padStart(2, "0")
+	} else{
+		return numero.toString()
+	}
 }
+
+function iniciarCronometro(){
+	if (!emExecucao){
+		emExecucao = true
+		intervalo = setInterval(function(){
+			cronometro.innerHTML =`${formatarNumero(horas)}:${formatarNumero(minutos)}:${formatarNumero(segundos)}`
+			segundos++
+			if (segundos<10){
+				segundos = `0${segundos}`
+			}
+			if (segundos>=60){
+				segundos = 0
+				minutos++
+			}
+			if(minutos>=60){
+				minutos = 0
+				horas++
+			}
+		},1000)
+	}
+}
+function pararCronometro(){
+	clearInterval(intervalo)
+	emExecucao = false
+}
+function resetarCronometro(){
+	clearInterval(intervalo)
+	emExecucao = false
+	segundos = 1
+	minutos = 0
+	horas = 0
+	cronometro.innerHTML = "00:00:00"
+} 
