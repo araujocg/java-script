c7a748f370eb5279f98d115962d137b71eb669c8
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-09 14:51:01 -0400

    forca-game 0.2 09/10/23

--- CursoJS/forca-game/script.js
+++ CursoJS/forca-game/script.js
@@ -6,14 +6,46 @@ const resposta = [
 ]
 const indiceAleatorio = Math.floor(Math.random() * resposta.length)
 const respostaSorteada = resposta[indiceAleatorio]
+console.log(respostaSorteada)
+const letterAlreadyClicked = []
 
-function check(letra){
-    if(resposta.some(props => props.nome.includes({letra}))){
+function foundPositionLetter(palavra, letra){
+    const position = []
+
+    for (let p = 0; p < palavra.length; p++){
+        if (palavra[p] === letra){
+            position.push(p)
+        }
+    }
+    return position
+}
+
+function alreadyClicked(letra){ // verificar se a letra ja foi clicada
+    return letterAlreadyClicked.includes(letra)
+}
+
+function check(letra){ // verificar se a letra esta na palavra
+    if (respostaSorteada.nome.includes(letra)){
         return true
     } else {
         return false
     }
 }
-console.log(respostaSorteada)
-console.log(check("a"))
-// checar se a letra esta na palavra
+
+
+function forcaFunction(letra){ // função principal
+    if (!alreadyClicked(letra)){
+        letterAlreadyClicked.push(letra)
+        if (check(letra)){
+            const position = foundPositionLetter(respostaSorteada.nome, letra) 
+            if (position.length > 0) {
+                window.alert(`${letra} foi encontrada nas posições ${position.join(', ')}`)
+            }
+        }
+    } else {
+        window.alert('Esta letra já foi clicada.')
+    }
+}
+
+//////// ESTA MOSTRANDO JA A POSIÇÃO DA LETRA EM QUESTÃO
+//////// AGR ACHO Q JA ESTA NA HORA DE COMEÇAR A COLOCAR A LETRA NA CORRETA NO PRA APARECER AO INVEZ DE MANDAR UM WINDOW.ALERT XD
