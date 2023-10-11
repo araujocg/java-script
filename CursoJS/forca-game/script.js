5fe40a588c9f0c347a787b075a2ff71aa0c8e804
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-11 14:39:21 -0400

    forca-game 0.3 11/10/23

--- CursoJS/forca-game/script.js
+++ CursoJS/forca-game/script.js
@@ -4,20 +4,34 @@ const resposta = [
     {nome:"lunar"},
     {nome:"leite"}
 ]
+
 const indiceAleatorio = Math.floor(Math.random() * resposta.length)
 const respostaSorteada = resposta[indiceAleatorio]
-console.log(respostaSorteada)
 const letterAlreadyClicked = []
+const divs = []
+console.log(respostaSorteada.nome)
 
-function foundPositionLetter(palavra, letra){
-    const position = []
+for (let i = 1; i <= respostaSorteada.nome.length; i++) {
+    divs.push(document.getElementById(`letra_resposta0${i}`));
+}
 
-    for (let p = 0; p < palavra.length; p++){
-        if (palavra[p] === letra){
-            position.push(p)
+function atualizarDivsComLetras(palavra, letra) {
+    const posicoes = foundPositionLetter(palavra, letra);
+    
+    for (const posicao of posicoes) {
+        divs[posicao].textContent = letra;
+    }
+}
+
+function foundPositionLetter(palavra, letra) {
+    const position = [];
+
+    for (let p = 0; p < palavra.length; p++) {
+        if (palavra[p] === letra) {
+            position.push(p);
         }
     }
-    return position
+    return position;
 }
 
 function alreadyClicked(letra){ // verificar se a letra ja foi clicada
@@ -37,15 +51,11 @@ function forcaFunction(letra){ // função principal
     if (!alreadyClicked(letra)){
         letterAlreadyClicked.push(letra)
         if (check(letra)){
-            const position = foundPositionLetter(respostaSorteada.nome, letra) 
-            if (position.length > 0) {
-                window.alert(`${letra} foi encontrada nas posições ${position.join(', ')}`)
-            }
+            atualizarDivsComLetras(respostaSorteada.nome, letra)
         }
     } else {
         window.alert('Esta letra já foi clicada.')
     }
 }
-
 //////// ESTA MOSTRANDO JA A POSIÇÃO DA LETRA EM QUESTÃO
 //////// AGR ACHO Q JA ESTA NA HORA DE COMEÇAR A COLOCAR A LETRA NA CORRETA NO PRA APARECER AO INVEZ DE MANDAR UM WINDOW.ALERT XD
