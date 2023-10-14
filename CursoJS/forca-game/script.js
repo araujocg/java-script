9551ae329c14283297391b7d666c9fa23cbbd832
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-14 11:23:32 -0400

    forca-game05 14/10/23

--- CursoJS/forca-game/script.js
+++ CursoJS/forca-game/script.js
@@ -9,16 +9,29 @@ const indiceAleatorio = Math.floor(Math.random() * resposta.length)
 const respostaSorteada = resposta[indiceAleatorio]
 const letterAlreadyClicked = []
 const divr = []
-console.log(respostaSorteada.nome)
-
 for (let i = 1; i <= respostaSorteada.nome.length; i++) {
     divr.push(document.getElementById(`letra_resposta0${i}`));
+}
+const divt = []
+for (let i = 1; i <= 26; i++) {
+    divt.push(document.getElementById(`letra_teclado${i}`));
 }
 //
 
 //
 
 //
+function atualizarDivtERRO(letra) {
+    const numero = letra.charCodeAt(0) - 97
+    divt[numero].style.color = '#6e0303'
+    divt[numero].style.background = '#7c7c7c'
+}
+function atualizarDivtCORRETO(letra) {
+    const numero = letra.charCodeAt(0) - 97
+    divt[numero].style.color = '#53c553'
+    divt[numero].style.background = '#7c7c7c'
+}
+
 function atualizarDivrComLetras(palavra, letra) {
     const posicoes = foundPositionLetter(palavra, letra);
     
@@ -68,7 +81,10 @@ function forcaFunction(letra){
         letterAlreadyClicked.push(letra)
         if (check(letra)){
             atualizarDivrComLetras(respostaSorteada.nome, letra)
-        } 
+            atualizarDivtCORRETO(letra)
+        } else{
+            atualizarDivtERRO(letra) 
+        }
     } else {
         window.alert('Esta letra já foi clicada.')
     }
