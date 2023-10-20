81c3fcb2f3845d1622add60fa7587c9b2f96b291
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-10-20 19:51:18 -0400

    aulas novas 20/10/23

--- CursoJS/aula32/index.js
+++ CursoJS/aula32/index.js
@@ -1,21 +1,22 @@
-const concoMil = () => Promise((resolve, reject) =>{
+const cincoMil = () => new Promise((resolve, reject) =>{
     setTimeout(() => resolve("promise cincoMil resolvida"), 5000)
 })
-const mil = () => Promise((resolve, reject) =>{
+const mil = () => new Promise((resolve, reject) =>{
     setTimeout(() => resolve("promise mil resolvida"), 1000)
 })
-const quinhentos = () => Promise((resolve, reject) =>{
+const quinhentos = () => new Promise((resolve, reject) =>{
     setTimeout(() => resolve("promise quinhentos resolvida"), 500)
 })
-const cem = () => Promise((resolve, reject) =>{
+const cem = () => new Promise((resolve, reject) =>{
     setTimeout(() => resolve("promise cem resolvida"), 100)
 })
 
-const resolvedoraDePromisesAssincronas = () => {
-    const resultado1 = cincoMil()
-    const resultado2 = quinhentos()
-    const resultado3 = cem()
-    const resultado4 = mil()
+const resolvedoraDePromisesAssincronas =  async()  => { // quando tem o async eu estou dizendo q a função q tem algum tipo de codigo rodando dentro dela q é assíncrono e que 
+    const resultado1 = await cincoMil() // await serve para o programar esperar concluir e ai ir pra proxima linha
+    const resultado2 = await quinhentos()
+    const resultado3 = await cem()
+    const resultado4 = await mil()
 
     console.log(`Mensagem: ${resultado1}, ${resultado2},  ${resultado3}, ${resultado4} `)
 }
+resolvedoraDePromisesAssincronas()
