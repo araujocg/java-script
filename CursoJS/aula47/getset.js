8530a346d2658b8bb1f502a9dc15e99e482c8c70
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-11-22 20:19:47 -0400

    aulas novas 22/11/23

--- CursoJS/aula47/getset.js
+++ CursoJS/aula47/getset.js
@@ -7,10 +7,10 @@ const obj1 = {
         this.listaString.unshift(item);
     }
 };
-console.log(obj1.primeiraString);
+console.log(obj1.primeiraString); // Inscreva-se
 delete obj1.primeiraString;
 obj1.primeiraString = "Deixa um like";
-console.log(obj1.primeiraString);
+console.log(obj1.primeiraString); // Deixa um like
 
 //
 
@@ -24,9 +24,9 @@ Object.defineProperty(obj2, "prop", {
         this.a = x;
     }
 });
-console.log(obj2.prop);
+console.log(obj2.prop); // 2
 obj2.prop = 5;
-console.log(obj2.prop);
+console.log(obj2.prop); // 5
 
 //
 
@@ -41,6 +41,41 @@ const obj = {
         this.a = novoValor;
     }
 }
-console.log(obj.computada);
+console.log(obj.computada); // abc
 obj.computada = "xyz";
-console.log(obj.computada);
+console.log(obj.computada); // xyz
+
+//
+
+class Usuario {
+    #primeiroNome = "Brazilian";
+    #segundoNome = "Dev";
+
+    get nomeCompleto(){
+        return `${this.#primeiroNome} ${this.#segundoNome}`
+    }
+    set nomeCompleto(value){
+        [this.#primeiroNome, this.#segundoNome] = value.split(" ");
+    }
+}
+
+const usuario = new Usuario();
+console.log(usuario.nomeCompleto); // Brazilian Dev
+usuario.nomeCompleto = "joão", "Almeida";
+console.log(usuario.nomeCompleto); // joão Almeida
+
+//
+
+class ClasseStringTeste {
+    static #strInterna = "uma string teste";
+    static get str(){
+        return this.#strInterna;
+    }
+    static set str(value){
+        this.#strInterna = value;
+    }
+}
+
+console.log(ClasseStringTeste.str); // uma string teste
+ClasseStringTeste.str = "teste de string";
+console.log(ClasseStringTeste.str); // teste de string
