b805405408f54863b168916b3cecbf3d3a2f191d
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-08-29 10:53:18 -0400

    nodejs 29/08/24

---
+++ NodeJS/01/nome.js
@@ -0,0 +1,4 @@
+const nome = "João";
+const sobrenome = "Araújo";
+
+console.log(`${nome} ${sobrenome}`);
---
+++ NodeJS/02/calc.js
@@ -0,0 +1,9 @@
+const soma = require("./somar");
+const div = require("./divisao");
+const mult = require("./multiplicacao");
+const sub = require("./subtracao");
+
+console.log(soma(10,2));
+console.log(div(10,2));
+console.log(mult(10,2));
+console.log(sub(10,2));
