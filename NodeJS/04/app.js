d6e472a64056f90fc5c5d5cd38f3e86ff69dee05
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-09-04 15:28:35 -0400

    nodejs 04/09/24

--- NodeJS/04/app.js
+++ NodeJS/04/app.js
@@ -2,17 +2,26 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("Problema do GET resolvido!")
+    // res.send("Problema do GET resolvido!")
+    
+    res.sendFile(__dirname + "/html/index.html") // __dirname é a rota do arquivo 
 });
 
 app.get("/sobre", function (req, res) {
-    res.send("Minha página Sobre");
+    // res.send("Minha página Sobre"); // send SÓ PODE ser enviado uma unica vez dentro da sua função de rota
+    // res.send("Essa mensagem não vai chegar a aparecer");
+
+    res.sendFile(`${__dirname}/html/sobre.html`)
 });
 
 app.get("/blog", function (req, res) {
-    res.send("Minha página blog")
+    res.send("Minha página blog");
 })
 
-app.listen(8081, function () {
+app.get("/ola/:nome/:cor", function(req,res){ // Quando colocar um parametro é obrigatório passá-lo 
+    res.send(`<h1>Olá ${req.params.nome}, Seja Bem Vindo! </h1> <h2 style="color:blue">Sua cor favorita é ${req.params.cor}</h2>`);
+})
+
+app.listen(8081, function () { // sempre a ultima linha de código
     console.log("Servidor rodando normalmente!")
-}); // sempre a ultima linha de código
+}); 
