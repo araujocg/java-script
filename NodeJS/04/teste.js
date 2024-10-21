a85a3f5cf94637061e65a0d033bb0146b3ff5c1c
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-10-21 16:30:43 -0400

    nodejs 21/10/24

--- NodeJS/04/teste.js
+++ NodeJS/04/teste.js
@@ -1,13 +1,51 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('test', 'root', 'Drakie1amantedemusicas',{
+const sequelize = new Sequelize('teste'/* Nome do Banco de dados*/, 'root', 'Drakie1amantedemusicas',{
     host: "localhost",
     dialect: "mysql"
 });
 
-sequelize.authenticate()
-    .then(function(){
-        console.log("Conectado com sucesso!");
-    })
-    .catch(function(err){
-        console.log(`Erro ao se conectar: ${err}`);
-    })
+// sequelize.authenticate()
+//     .then(function(){
+//         console.log("Conectado com sucesso!");
+//     })
+//     .catch(function(err){
+//         console.log(`Erro ao se conectar: ${err}`);
+//     })
+
+const Posts = sequelize.define('posts',{
+    title: {
+        type: Sequelize.STRING,
+    },
+    content: {
+        type: Sequelize.TEXT
+    }
+})
+
+// Posts.create({
+//     title: "CORALINE",
+//     content: "terror, suspense, stop Motion"    only create
+// })
+
+const User = sequelize.define('users', {
+    name: {
+        type: Sequelize.STRING
+    },
+    surName: {
+        type: Sequelize.STRING
+    },
+    age: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+User.create({
+    name: "Guilherme",
+    surName: "Araújo Magalhães",
+    age: 19,
+    email: "Draki.araujo@gmail.com"
+})
+
+// User.sync({force:true}); create a table
---
+++ NodeJS/05/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars')
+
+app.listen(8081, function(){
+    console.log("Servidor Rodando!");
+});
