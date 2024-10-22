d5888c5f355c8398d64c9d05a90d28f4d3915103
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-10-22 19:15:35 -0400

    nodejs 22/10/24

--- NodeJS/05/index.js
+++ NodeJS/05/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
+
+
+// config
+    // Template Engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    // Connection with database MySql
+    const sequelize = new Sequelize('teste'/* Nome do Banco de dados*/, 'root', 'Drakie1amantedemusicas',{
+        host: "localhost",
+        dialect: "mysql"
+    });
+
+// Routes
+    app.get('/cad', function(req,res){
+        res.render('form')
+    })
 
 app.listen(8081, function(){
     console.log("Servidor Rodando!");
