1a3132dfb28e27391096d58585d6e8c3eddc31a0
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-11-08 15:54:02 -0400

    nodejs 08/11/24

--- NodeJS/mongo/index.js
+++ NodeJS/mongo/index.js
@@ -1,9 +1,56 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/mongo")
-    .then(()=>{
+// config mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/mongo", {
+    useMongoClient: true
+})
+    .then(() => {
         console.log("Conected");
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log("ERROR:" + err);
+    });
+// model
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    surname: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    country: {
+        type: String
+    }
+});
+
+mongoose.model('users', UserSchema);
+
+
+
+const User = mongoose.model('users');
+new User({
+    name: 'Bartolomeu',
+    surname: 'Almeida',
+    email: 'Batolas123@gmail.com',
+    age: 42,
+    country: 'Portugal'
+})
+    .save()
+    .then(() => {
+        console.log("User created");
+    })
+    .catch((err) => {
+        console.log("ERROR" + err);
     })
