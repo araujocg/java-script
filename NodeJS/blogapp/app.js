450ccabee2abdacf8347d9cdae015822f2d6945e
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-12-16 15:05:02 -0400

    nextjs 16/12/24

--- NodeJS/blogapp/app.js
+++ NodeJS/blogapp/app.js
@@ -4,10 +4,29 @@ const bodyParser = require("body-parser");
 const admin = require("./routes/admin");
 const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const app = express();
 
 // Config
+// Session
+
+    app.use(session({
+        secret: "NODEJS",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash());
+
+// MiddleWare
+
+    app.use((req,res,next)=>{
+        res.locals.success_msg = req.flash("success_msg");
+        res.locals_error_msg = req.flash("error_msg");
+        next()
+    })
+
 // HandleBars
 
     app.engine('handlebars', handlebars.engine({
